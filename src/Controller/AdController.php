c6458205b60f7457fef5002e561d5f207c6d9682
Elmardi2 <mardi.yahia@gmail.com>
2018-12-12 16:02:12 +0100

    Add form and subform of Announces

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Ad;
 use App\Form\AdType;
+use App\Entity\Image;
 use App\Repository\AdRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -40,6 +41,7 @@ class AdController extends AbstractController
     public function create(Request $request, ObjectManager $manager)
     {
         $ad = new Ad();
+
         $form = $this->createForm(AdType::class, $ad);
 
         $form->handleRequest($request);
--- src/Form/AdType.php
+++ src/Form/AdType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Ad;
+use App\Form\ImageType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -11,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class AdType extends AbstractType
 {
@@ -33,13 +35,41 @@ class AdType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextType::class, $this->getConfig("Title", "Type your title here"))
-            ->add('slug', TextType::class, $this->getConfig("Address web", "It will be taken automatically"))
-            ->add('introduction', TextType::class, $this->getConfig("Introduction", "Type the best introduction to your house"))
-            ->add('description', TextareaType::class,$this->getConfig("Description", "Type the best description to your house"))
-            ->add('coverImage', UrlType::class, $this->getConfig("Cover Image", "Put best cover image to your annonce"))
-            ->add('rooms', IntegerType::class, $this->getConfig("Rooms's number", "Type how many rooms you have here"))
-            ->add('price', MoneyType::class, $this->getConfig("Price", "Type how much you whish as price"));
+            ->add(
+                'title', TextType::class, $this->getConfig("Title", "Type your title here")
+                )
+            ->add(
+                    'slug', TextType::class, $this->getConfig(
+                    "Address web", "It will be taken automatically")
+                    )
+            ->add(
+                'introduction', TextType::class, $this->getConfig(
+                    "Introduction", "Type the best introduction to your house")
+                    )
+            ->add(
+                'description', TextareaType::class,$this->getConfig("
+                Description", "Type the best description to your house")
+                )
+            ->add(
+                'coverImage', UrlType::class, $this->getConfig(
+                    "Cover Image", "Put best cover image to your annonce")
+                    )
+            ->add(
+                'rooms', IntegerType::class, $this->getConfig(
+                    "Rooms's number", "Type how many rooms you have here")
+                    )
+            ->add(
+                'price', MoneyType::class, $this->getConfig(
+                    "Price", "Type how much you wish as price")
+                )
+                ->add(
+                    'images', CollectionType::class, 
+                    [
+                        'entry_type' => ImageType::class,
+                        'allow_add'  => true
+                    ]
+                )
+                ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
